999f3caa657b343082843553aa679812847b317a
theswiftintern <theswiftintern@gmail.com>
2022-08-01 10:32:53 +0800

    Initial Commit

---
+++ Unit Conversion/Unit_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConversionApp.swift
+//  Unit Conversion
+//
+//  Created by RJ Tedoco on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
